a35c57009f84b685119fc5948247b579d54294ed
Arun HK <arunhk002600@gmail.com>
2026-09-23 22:57:26 +0530

    LeetCode 1929 - Concatenation of Array solution

---
+++ python/1929->concatenation-of-array.py
@@ -0,0 +1,10 @@
+# LeetCode 1929 - Concatenation of Array
+# Difficulty: Easy
+# Status: Accepted
+# Topic: Array
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums + nums
